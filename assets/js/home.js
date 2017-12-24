757383a0757bc7793b5a94567d2d579e1f62bd4e
Shadwell Da Cunha <shadwelldacunha@gmail.com>
2017-12-24 19:46:25 +1100

    Add navbar

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,21 +1,25 @@
-function applyHeaderBlur(header, scrollTop, pivotPoint) {
-    header.css('filter', 'blur(' + scrollTop / 2 + 'px)');
+function applyHeaderBlur(navbar, header, scrollTop, pivotPoint) {
 
     if (scrollTop > pivotPoint) {
         header.css('opacity', '0');
-        header.css('top', '-' + pivotPoint + 'px');
+        navbar.css('top', '0px');
+        navbar.addClass('fixed');
     } else {
         header.css('opacity', '1');
+        header.css('filter', 'blur(' + scrollTop / 2 + 'px)');
         header.css('top', '-' + scrollTop + 'px');
+        navbar.css('top', (pivotPoint - scrollTop) + 'px');
+        navbar.removeClass('fixed');
     }
 }
 
 $(document).ready(function () {
     var scrollTop = $(window).scrollTop();
+    var navbar = $('.navbar');
     var header = $(".hero-collapsing-header");
-    var pivotPoint = ($(window).height() * 0.8) - 65;
+    var pivotPoint = ($(window).height() * 0.8);
 
-    applyHeaderBlur(header, scrollTop, pivotPoint);
+    applyHeaderBlur(navbar, header, scrollTop, pivotPoint);
 
     $('.project-items').slick({
         autoplay: true,
@@ -39,8 +43,9 @@ $(document).ready(function () {
 });
 
 $(window).scroll(function () {
+    var navbar = $('.navbar');
     var scrollTop = $(window).scrollTop();
     var header = $(".hero-collapsing-header");
-    var pivotPoint = ($(window).height() * 0.8) - 65;
-    applyHeaderBlur(header, scrollTop, pivotPoint);
+    var pivotPoint = ($(window).height() * 0.8);
+    applyHeaderBlur(navbar, header, scrollTop, pivotPoint);
 });
